493d1ea7ee26dc8ee78071b7b39566ab04dd9c9d
Daniel França <daniel.franca@gmail.com>
2016-06-14 15:58:07 +0200

    added title and month navigation

--- app/components/BudgetList.js
+++ app/components/BudgetList.js
@@ -3,9 +3,22 @@ import BudgetItem from '../components/BudgetItem'
 
 function BudgetList(props) {
     var budgetItems = props.budgetItems;
+    var year = props.year;
+    var month = props.month;
+
     return (
         <div>
-            <h1>2016 June</h1>
+            <div className="ui buttons">
+                <button className="ui button">
+                <i className="chevron left icon blue"></i>
+                </button>
+                <button className="ui button blue">
+                    {year} {month}
+                </button>
+                <button className="ui button">
+                <i className="chevron right icon blue"></i>
+                </button>
+            </div>
             <div className="ui middle aligned divided list">
                 {budgetItems.map(function(budgetItem) {
                   return <div className="item" key={budgetItem.id}>
