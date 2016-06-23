bec8ec561735ed7feb719ba7bb5e5d771c52e7e1
Daniel França <daniel.franca@gmail.com>
2016-06-23 19:56:58 +0200

    changed edit button to show icon instead of text/added new delete button

--- app/components/BudgetItem.js
+++ app/components/BudgetItem.js
@@ -6,7 +6,8 @@ function BudgetItem(props) {
     var iconClass = "circular " + guessBestIcon(props.budgetItem.category) + " icon blue";
     return  <div className="content">
             <div className="right floated content">
-                <div className="ui button blue">Edit</div>
+                <div className="ui icon button blue"><i className="icon edit"></i></div>
+                <div className="ui icon button blue"><i className="icon trash"></i></div>
             </div>
             <div className="header" style={{color: "#555555"}}>
                 <i className={iconClass}></i>
